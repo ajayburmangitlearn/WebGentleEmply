66f073033bfff8d4ba3e2f77fec174b847e9e27b
ajaykumar <mail2.kumarajay@gmail.com>
2022-08-01 23:17:03 +0530

    mESSAGE changed

--- WebGentleEmply/Startup.cs
+++ WebGentleEmply/Startup.cs
@@ -32,7 +32,7 @@ namespace WebGentleEmply
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World AJAY!");
                 });
             });
         }
